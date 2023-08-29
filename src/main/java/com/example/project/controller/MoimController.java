ed8bbc72ef44983c7c62a4b75ea15707e6453bec
Knaeunni <nanizz9106@gmail.com>
2023-08-29 13:57:28 +0900

    2023829 2시까지

--- src/main/java/com/example/project/controller/MoimController.java
+++ src/main/java/com/example/project/controller/MoimController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Main {
-    @GetMapping("/")
-    public String main(){
-        return "/html/main";
+public class MoimController {
+    @GetMapping("/html/moim")
+    public String moim(){
+        return "/html/moim";
     }
 }
