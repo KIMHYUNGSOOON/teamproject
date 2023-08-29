a32c646e3cffa932c7418355aafa1355744f20fe
Knaeunni <nanizz9106@gmail.com>
2023-08-29 18:25:30 +0900

    2023829 회원가입빼고 다됨

--- src/main/java/com/example/project/controller/LoginController.java
+++ src/main/java/com/example/project/controller/LoginController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Main {
-    @GetMapping("/")
-    public String main(){
-        return "/html/index";
+public class LoginController {
+    @GetMapping("/html/Login")
+    public String lodging(){
+        return "/html/Login";
     }
 }
--- src/main/java/com/example/project/controller/MemberController.java
+++ src/main/java/com/example/project/controller/MemberController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Main {
-    @GetMapping("/")
-    public String main(){
-        return "/html/index";
+public class MemberController {
+    @GetMapping("/html/Member")
+    public String lodging(){
+        return "/html/Member";
     }
 }
